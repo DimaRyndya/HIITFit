0c4631d90305ec1dee63ea3c16d473ec4386dd5e
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-08-28 18:26:23 +0300

    Add buttons and timer to ExerciseView

---
+++ HIITFit/ExersiceView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ExersiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExersiceView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExersiceView()
+    }
+}
